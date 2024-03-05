af7aa48604d42023fb1febf7828f6220b77e711d
Davinder9297 <davindergiri765@gmail.com>
2024-03-05 21:59:05 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -13,13 +13,19 @@ function App() {
 	const [show, setshow] = useState(false)
 	const [disable, setdisable] = useState(true)
 	const [getEdit, setEdit] = useState([])
+	const [duringSearchdata, setduringSearchdata] = useState([])
+	const [duringSearchdata1, setduringSearchdata1] = useState([])
+	const [duringSearchdata2, setduringSearchdata2] = useState([])
+	const [Data, setData] = useState([])
 	useEffect(() => {
 		async function Fetchdata() {
+			setshow(true)
 			let url = BASE_URL + 'students'
 			const data = await fetch(url)
 			const response = await data.json()
-			// console.log(response);
+			setshow(false)
 			setrowsData(response.data)
+			setData(response.data)
 		}
 		Fetchdata()
 	}, [])
@@ -43,6 +49,9 @@ function App() {
 	}
 	const deleteTableRows = (index) => {
 		let url = BASE_URL + 'deletestudent'
+		try {
+			setshow(true)
+		
 		let data = {
 			id: rowsData[index]._id,
 		}
@@ -69,7 +78,8 @@ function App() {
 					error
 				)
 			})
-
+			setshow(false)
+		
 		setdisable(false)
 		setsave('cursor-pointer')
 		const rows = [...rowsData]
@@ -79,6 +89,9 @@ function App() {
 		if (rowsData.length == 1) {
 			setnorecord('')
 		}
+	} catch (error) {
+		console.log(error);
+	}
 	}
 
 	const handleChange = async (index, evnt) => {
@@ -93,11 +106,12 @@ function App() {
 		}
 		setrowsData(rowsInput)
 	}
-	function Save() {
-		console.log(rowsData)
-	}
+
 
 	function handleEdit(index) {
+		try {
+			setshow(true)
+		
 		if (getEdit.findIndex((data) => data == index) == -1) {
 			let temp = [...getEdit]
 			temp.push(index)
@@ -139,7 +153,9 @@ function App() {
 					})
 			} else {
 				let url = BASE_URL + 'registerstudent'
-				console.log(rowsData[index - 1]);
+				// console.log(rowsData[index - 1]);
+				
+
 				fetch(url, {
 					method: 'POST',
 					headers: {
@@ -162,78 +178,157 @@ function App() {
 							error
 						)
 					})
+					
+
 			}
 		}
+		setshow(false)
+	} catch (error) {
+			console.log(error);
+	}
 	}
 
-	async function handleSearch(regDate, stdName, homeCity) {
+	async function handleSearchcity(homeCity) {
+		if(homeCity==''){
+			setduringSearchdata1([])
+			setrowsData(Data)
+		}
+		else{
+			setshow(true)
+
 		let url =
 			BASE_URL +
-			`students?regDate=${regDate}&stdName=${stdName}&homeCity=${homeCity}`
+			`students?homeCity=${homeCity}`
 		const data = await fetch(url)
 		const response = await data.json()
 		// console.log(response);
+		setshow(false)
+
+		setduringSearchdata1(response.data)
 		setrowsData(response.data)
+		}
+		// let query=e.target.value;
+        // console.log(query);
+        
+	}
+	
+	function Searchbystdname(e){
+		let query=e.target.value;
+        // console.log(query);
+        if(query==''){
+            setduringSearchdata([])
+			// console.log("query:",Data);
+			setrowsData(Data)
+        }
+       setduringSearchdata(rowsData.filter((item)=>{
+            const searchitem=query.toLowerCase()
+              const stdName=item.stdName.toLowerCase()
+
+              return searchitem && (stdName.includes(searchitem));
+        }))
+	}
+	function Searchbydate(e){
+		let query=e.target.value;
+        // console.log(query);
+        if(query==''){
+            setduringSearchdata2([])
+			// console.log("query:",Data);
+			setrowsData(Data)
+        }
+       setduringSearchdata2(rowsData.filter((item)=>{
+            const searchitem=query.toLowerCase()
+              const regDate=item.regDate.toLowerCase()
+
+              return searchitem && (regDate.includes(searchitem));
+        }))
 	}
 
+
 	async function clearFilers() {
+		setshow(true)
 		let url = BASE_URL + 'students'
 		const data = await fetch(url)
 		const response = await data.json()
+		setshow(false)
 		// console.log(response);
 		setrowsData(response.data)
 	}
 
+	function SetDataInTable(item){
+		// console.log(item);
+		setduringSearchdata([])
+		setduringSearchdata1([])
+		setduringSearchdata2([])
+		setrowsData([item])
+	}
 	return (
 		<>
 			<div className="w-full min-h-screen flex justify-center items-center font-mons ">
 				<div
 					name=""
-					className="w-[90%] min-h-[30vh] max-h-[90vh] overflow-y-auto tablepstd"
+					className="w-[90%] min-h-[30vh] max-h-[90vh] overflow-y-auto tabledata"
 				>
-					<div className="flex justify-end w-full mb-4">
-						<div className="relative">
-							<IoSearch className="absolute h-5 w-5 top-2 left-2" />
-							<input
-								placeholder="Search data by any parameter.."
-								className="outline-none py-1 w-80 border border-[#945a28] rounded pl-8 focus:shadow-inner bg-[#ece7e2]"
-							/>
-						</div>
-					</div>
 					<div className="flex justify-between items-center">
 						<div className="relative">
 							<IoSearch className="absolute h-5 w-5 top-2 left-2" />
 							<input
-								onChange={(evnt) =>
-									// handleSearch(regDate, stdName, homeCity)
-									handleSearch('', evnt.target.value, '')
-								}
+							type='text'
+								onChange={Searchbystdname}
 								placeholder="Filter by student name.."
 								className="outline-none py-1 w-60 border border-[#945a28] rounded pl-8 focus:shadow-inner bg-[#ece7e2]"
 							/>
+							<div className='h-auto min-h-0 w-60 flex flex-col z-20 absolute bg-[#ece7e2] '>
+								{
+									duringSearchdata.map((item,ind)=>{
+									
+										return(<>
+								<div key={ind} onClick={()=>{SetDataInTable(item)}} className='cursor-pointer text-center py-1 px-1 h-auto border border-gray-400 '>{item.stdName}</div>
+
+										</>)
+									})
+								}
+
+							</div>
 						</div>
 						<div className="relative">
 							<IoSearch className="absolute h-5 w-5 top-2 left-2" />
 							<input
-								onChange={(evnt) =>
-									// handleSearch(regDate, stdName, homeCity)
-									handleSearch('', '', evnt.target.value)
-								}
-								placeholder="Filter by home address.."
+							type='text'
+								onChange={(e)=>handleSearchcity(e.target.value)}
+								placeholder="Filter by home city.."
 								className="outline-none py-1 w-60 border border-[#945a28] rounded pl-8 focus:shadow-inner bg-[#ece7e2]"
 							/>
+							<div className='h-auto min-h-0 w-60 flex flex-col z-20 absolute bg-[#ece7e2] '>
+								{
+									duringSearchdata1.map((item,ind)=>{
+										
+										return(<>
+								<div key={ind} onClick={()=>{SetDataInTable(item)}} className='cursor-pointer text-center py-1 px-1 h-auto border border-gray-400 '>{item.stdName}</div>
+
+										</>)
+									})
+								}
+								</div>
 						</div>
 						<div className="relative">
 							<IoSearch className="absolute h-5 w-5 top-2 left-2" />
 							<input
-								onChange={(evnt) =>
-									// handleSearch(regDate, stdName, homeCity)
-									handleSearch(evnt.target.value, '', '')
-								}
+								onChange={Searchbydate}
 								type="date"
 								placeholder="Filter by date.."
 								className="outline-none py-1 w-60 border border-[#945a28] rounded pl-8 focus:shadow-inner bg-[#ece7e2]"
 							/>
+							<div className='h-auto min-h-0 w-60 flex flex-col z-20 absolute bg-[#ece7e2] '>
+								{
+									duringSearchdata2.map((item,ind)=>{
+										
+										return(<>
+								<div key={ind} onClick={()=>{SetDataInTable(item)}} className='cursor-pointer text-center py-1 px-1 h-auto border border-gray-400 '>{item.stdName}</div>
+
+										</>)
+									})
+								}
+								</div>
 						</div>
 
 						<div className="flex justify-end space-x-2 text-white ">
@@ -241,20 +336,13 @@ function App() {
 								onClick={clearFilers}
 								className="bg-amber-800 p-2 rounded"
 							>
-								Clear
-							</button>
-							<button
-								disabled={disable}
-								className={`bg-yellow-400 p-2 rounded ${save}`}
-								onClick={Save}
-							>
-								Save Changes
+								Clear Searched Data
 							</button>
 							<button
 								onClick={addTableRows}
 								className="bg-amber-800 p-2 rounded"
 							>
-								+Add Record
+								+Add New Record
 							</button>
 						</div>
 					</div>
